aff29cdc0395bde49b3142be378aeac2505e3f50
ZK-Elite <zkdev.elite@gmail.com>
2024-05-28 17:43:36 +0530

    News Page UI development

---
+++ client/src/app/news/page.tsx
@@ -0,0 +1,102 @@
+"use client";
+
+import React, { useCallback, useEffect, useState } from "react";
+import { useSearchParams } from "next/navigation";
+import { GoogleSearchResponse } from "../../data/googletypes";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "../../components/ui/carousel";
+
+import { SkeletonForPage } from "@/src/components/Skeleton";
+import NewsVideoCard from "@/src/components/news-video";
+export default function Page() {
+  const searchParams = useSearchParams();
+  const query = searchParams.get("q");
+  const [result, setResult] = useState<GoogleSearchResponse>();
+
+  const [loading, setLoading] = useState(true);
+
+  const fetchData = useCallback(async (endpoint: any, options: any) => {
+    const response = await fetch(endpoint, options);
+    if (!response.ok) {
+      throw new Error(
+        `Failed to fetch data from server: ${response.statusText}`
+      );
+    }
+    return await response.json();
+  }, []);
+
+  const loadData = useCallback(async () => {
+    setLoading(true);
+    try {
+      const apiCalls = [
+        fetchData("/api/google", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ query }),
+        }),
+      ];
+      const [googleData] = await Promise.all(apiCalls);
+
+      setResult(googleData.data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    } finally {
+      setLoading(false);
+    }
+  }, [fetchData, query]);
+
+  useEffect(() => {
+    if (query) {
+      loadData();
+    }
+  }, [loadData, query]);
+
+  return (
+    <>
+      <div className="flex flex-col items-center md:space-auto space-y-2">
+        <div className="bottom-0 w-full flex justify-center sm:mt-[10rem] mt-[13rem] flex-col xl:flex-row mb-[8.5rem] sm:px-9 px-5 gap-8">
+          <div className="flex-auto w-full xl:w-7/12">
+            <div className="p-4 content-group-right-first ">
+              {loading ? (
+                <SkeletonForPage />
+              ) : (
+                <>
+                  <Carousel className="flex flex-wrap ">
+                    <CarouselContent className="flex flex-wrap justify-start gap-y-4">
+                      {result &&
+                        result?.items.map((news, index) => {
+                          return (
+                            news.pagemap?.cse_image && (
+                              <CarouselItem
+                                className="md:basis-1/2 lg:basis-[20%] items-stretch  "
+                                key={index}
+                              >
+                                <NewsVideoCard
+                                  videoUrl={news.pagemap.cse_image[0].src}
+                                  title={news.title}
+                                  url={news.link}
+                                  site="News"
+                                />
+                              </CarouselItem>
+                            )
+                          );
+                        })}
+                    </CarouselContent>
+                  </Carousel>
+                  <div className="w-full flex justify-center items-center mt-9">
+                    <button className="flex flex-row items-center gap-2 bg-[#20292d] dark:bg-[#d3e8eb] text-white dark:text-black rounded-full py-3 px-36">
+                      Load more
+                    </button>
+                  </div>
+                </>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ client/src/components/news-video.tsx
@@ -0,0 +1,61 @@
+import Image from "next/image";
+import React from "react";
+
+interface VideoCardProps {
+  videoUrl: string;
+  title: string;
+  url: string;
+  site: string;
+}
+interface CustomLoaderProps {
+  src: string;
+  width: number;
+  quality?: number;
+}
+
+const customLoader = ({ src }: CustomLoaderProps) => {
+  return src;
+};
+
+const NewsVideoCard: React.FC<VideoCardProps> = ({
+  videoUrl,
+  title,
+  url,
+  site,
+}) => {
+  const [error, setError] = React.useState(false);
+
+  const handleImgError = () => {
+    setError(true);
+  };
+
+  return (
+    <div className="flex flex-col dark:bg-[#d3e8eb] bg-[#081d26] rounded-xl p-3 items-stretch h-full justify-between gap-2">
+      <a href={url} className="w-full">
+        <Image
+          className="w-full md:h-[160px] rounded-lg bg-cover ease-in-out hover:transition-all object-cover aspect-square"
+          loader={customLoader}
+          src={error ? "images/icons/placeholder-img.svg" : videoUrl}
+          alt={title}
+          width={500}
+          height={500}
+          onError={handleImgError}
+          unoptimized
+        />
+      </a>
+      <p className="text-base font-medium leading-5 text-white dark:text-black">
+        {title}
+      </p>
+      <div className="flex flex-row justify-between items-center">
+        <p className="text-sm font-light leading-5 text-[#D1D5DB] dark:text-black">
+          {site}
+        </p>
+        <p className="text-sm font-light leading-5 text-[#D1D5DB] dark:text-black">
+          23/05/24
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default NewsVideoCard;
