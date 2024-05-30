16a06b9149086938eb142a3d08c195a33355fa1b
ZK-Elite <zkdev.elite@gmail.com>
2024-05-30 13:08:47 +0530

    feat: add news

--- client/src/app/api/query/route.ts
+++ client/src/app/api/query/route.ts
@@ -1,55 +1,57 @@
-import { NextResponse } from 'next/server';
+import { NextResponse } from "next/server";
 
 interface SearchRequestBody {
-    keywords: string;
-    result?: number;
+  keywords: string;
+  result?: number;
 }
 
 interface TextResponse {
-    data: any;
+  data: any;
 }
 
 export async function POST(req: Request): Promise<Response> {
-    try {
-        const json: SearchRequestBody = await req.json();
-        const { keywords, result = 20 } = json;
+  try {
+    const json: SearchRequestBody = await req.json();
+    const { keywords, result = 20 } = json;
 
-        if (!keywords) {
-            return NextResponse.json(
-                { error: 'Keywords are required' },
-                { status: 400 }
-            );
-        }
-
-        const base_url = `${process.env.ZKSEARCH_BACKEND}/api/search/query`;
-
-        const textResponse = await fetch(base_url, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                keywords: keywords,
-                max_results: result
-            })
-        });
-
-        if (!textResponse.ok) {
-            throw new Error(`Error: ${textResponse.status}\n${textResponse.statusText}`);
-        }
-
-        const results: TextResponse = await textResponse.json();
-
-        return NextResponse.json(
-            { data: results },
-            { status: 200, headers: { 'Content-Type': 'application/json' } }
-        );
-    } catch (error) {
-        console.error('Error fetching Duckduckgo Query Search :', error);
-
-        return NextResponse.json(
-            { error: (error as Error).message },
-            { status: 500 }
-        );
+    if (!keywords) {
+      return NextResponse.json(
+        { error: "Keywords are required" },
+        { status: 400 }
+      );
     }
+
+    const base_url = `${process.env.ZKSEARCH_BACKEND}/api/search/query`;
+
+    const textResponse = await fetch(base_url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        keywords: keywords,
+        max_results: result,
+      }),
+    });
+
+    if (!textResponse.ok) {
+      throw new Error(
+        `Error: ${textResponse.status}\n${textResponse.statusText}`
+      );
+    }
+
+    const results: TextResponse = await textResponse.json();
+
+    return NextResponse.json(
+      { data: results },
+      { status: 200, headers: { "Content-Type": "application/json" } }
+    );
+  } catch (error) {
+    console.error("Error fetching Duckduckgo Query Search :", error);
+
+    return NextResponse.json(
+      { error: (error as Error).message },
+      { status: 500 }
+    );
+  }
 }
